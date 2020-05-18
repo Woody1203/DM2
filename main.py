3929dc78b00372b821d32e71119583db52adbbeb
atytgat <alextytgat@gmail.com>
2020-05-18 14:29:43 +0200

    added file baseline to test it in main

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.model_selection import KFold
 from ItemRank import itemrank
 from test0511 import ub_knn_test
+from baseline import baseline, baseline_2
 
 def compute_MSE(np_ratings, np_preds):
     ratings_flat = np_ratings.flatten()
@@ -34,27 +35,6 @@ def compute_MAE(np_ratings, np_preds):
 
     return mae_tot/nb_ratings
 
-def baseline(A, B):
-    A = pd.pivot_table(A,values='rating',index='userId',columns='movieId')
-    A.loc['mean'] = A.mean()
-    print(A)
-    data = A.drop(['mean'])
-    nmovie, nuser = B.shape
-    pred = np.zeros((nmovie,nuser))
-    # print(nmovie, nuser)
-
-    list_loc = list(B.stack().index)
-    print("this is A[loc]", A.loc['mean'])
-    mark = 0
-    for i,j in list_loc:
-        mark += 1
-        print("this is mark", mark)
-        print("this is i", i)
-        print("this is j", j)
-        print("this is pred[i-1][j-1]", pred[i-1][j-1])
-        print("this is A.loc['mean'][j]", A.loc['mean'][j])
-        pred[i-1][j-1] = A.loc['mean'][j]
-    return pred
 
 def kfold_cross_validation():
     "This data set consists of:\
@@ -74,6 +54,11 @@ def kfold_cross_validation():
     mse_test_ubknn = np.zeros(k)
     mae_test_ubknn = np.zeros(k)
 
+    mse_train_baseline = np.zeros(k)
+    mae_train_baseline = np.zeros(k)
+    mse_test_baseline = np.zeros(k)
+    mae_test_baseline = np.zeros(k)
+
     kf = KFold(n_splits=k)
     i = 0
     for train, test in kf.split(links_df):
@@ -88,6 +73,7 @@ def kfold_cross_validation():
         train_set = train_set.reindex(list(range(movie_ratings_df.T.index.min(),movie_ratings_df.T.index.max()+1)),fill_value=np.NaN, axis='columns')
         train_set = train_set.reindex(list(range(movie_ratings_df.index.min(),movie_ratings_df.index.max()+1)),fill_value=np.NaN)
         train_set = train_set.astype(float)
+        train_set_zeros = train_set.fillna(0) # NaN -> 0
 
 
         # test set : create the rating matrix and add the missing columns and rows; missing values are replaced by 0
@@ -96,10 +82,28 @@ def kfold_cross_validation():
         test_set = test_set.reindex(list(range(movie_ratings_df.index.min(),movie_ratings_df.index.max()+1)),fill_value=0)
         test_set = test_set.astype(float)
 
+        ### Baseline algorithm prediction
+        prediction_baseline = baseline(train_set_links,movie_ratings_df)
+        print(prediction_baseline)
+
+        # performance evaluation on the training set
+        mse_train_baseline[i] = compute_MSE(train_set_zeros.to_numpy(), prediction_baseline)
+        mae_train_baseline[i] = compute_MAE(train_set_zeros.to_numpy(), prediction_baseline)
+        print(mse_train_baseline[i])
+        print(mae_train_baseline[i])
+        print()
+
+        # performance evaluation on the test set
+        print(test_set.to_numpy())
+        mse_test_baseline[i] = compute_MSE(test_set.to_numpy(), prediction_baseline)
+        mae_test_baseline[i] = compute_MAE(test_set.to_numpy(), prediction_baseline)
+        print(mse_test_baseline[i])
+        print(mae_test_baseline[i])
+        print()
+
 
         ### Itemrank prediction
-        train_set_zeros = train_set.fillna(0) # NaN -> 0
-        train_set_zeros_copy = pd.DataFrame.copy(train_set_zeros)
+        # train_set_zeros_copy = pd.DataFrame.copy(train_set_zeros)
         # prediction_itemrank = itemrank(train_set_zeros_copy, 0.85 , 0.0001)
         # print(prediction_itemrank)
         #
@@ -118,28 +122,40 @@ def kfold_cross_validation():
         # print()
 
 
-        ### User-based knn prediction
-        train_set_copy = pd.DataFrame.copy(train_set)
-        print(movie_ratings_df)
-        print(train_set_links)
-        prediction_ubknn, testing = ub_knn_test(train_set_links, movie_ratings_df, 10)
-        print(prediction_ubknn)
-
-        # performance evaluation on the training set
-        mse_train_ubknn[i] = compute_MSE(train_set_zeros.to_numpy(), prediction_ubknn)
-        mae_train_ubknn[i] = compute_MAE(train_set_zeros.to_numpy(), prediction_ubknn)
-        print(mse_train_ubknn[i])
-        print(mae_train_ubknn[i])
-        print()
-
-        # performance evaluation on the test set
-        mse_test_ubknn[i] = compute_MSE(test_set.to_numpy(), prediction_ubknn)
-        mae_test_ubknn[i] = compute_MAE(test_set.to_numpy(), prediction_ubknn)
-        print(mse_test_ubknn[i])
-        print(mae_test_ubknn[i])
-        print()
+        # ### User-based knn prediction
+        # train_set_copy = pd.DataFrame.copy(train_set)
+        # print(movie_ratings_df)
+        # print(train_set_links)
+        # prediction_ubknn, testing = ub_knn_test(train_set_links, movie_ratings_df, 10)
+        # print(prediction_ubknn)
+        #
+        # # performance evaluation on the training set
+        # mse_train_ubknn[i] = compute_MSE(train_set_zeros.to_numpy(), prediction_ubknn)
+        # mae_train_ubknn[i] = compute_MAE(train_set_zeros.to_numpy(), prediction_ubknn)
+        # print(mse_train_ubknn[i])
+        # print(mae_train_ubknn[i])
+        # print()
+        #
+        # # performance evaluation on the test set
+        # mse_test_ubknn[i] = compute_MSE(test_set.to_numpy(), prediction_ubknn)
+        # mae_test_ubknn[i] = compute_MAE(test_set.to_numpy(), prediction_ubknn)
+        # print(mse_test_ubknn[i])
+        # print(mae_test_ubknn[i])
+        # print()
 
         i += 1
+        break
+
+    # mean results for ubknn
+    mean_mse_train_baseline = mse_train_baseline.mean()
+    mean_mae_train_baseline = mae_train_baseline.mean()
+    print(mean_mse_train_baseline)
+    print(mean_mae_train_baseline)
+
+    mean_mse_test_baseline = mse_test_baseline.mean()
+    mean_mae_test_baseline = mae_test_baseline.mean()
+    print(mean_mse_test_baseline)
+    print(mean_mae_test_baseline)
 
     # mean results for itemrank
     mean_mse_train_itemrank = mse_train_itemrank.mean()
@@ -167,3 +183,4 @@ def kfold_cross_validation():
 
 if __name__ == '__main__':
     kfold_cross_validation()
+
